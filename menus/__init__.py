e89b558f28ac91264f502f8f6e9f8212a30e0939
Apoorv Kansal <akansal@zendesk.com>
2018-01-07 12:52:05 +1100

    Organised code so tests can be added for menu validation.

---
+++ menus/__init__.py
@@ -0,0 +1,3 @@
+from .api_handler import ( parse_menus_data )
+from .validator import ( validate_menus )
+from .renderer import ( render_aggregate_menus_json )
---
+++ menus/api_handler.py
@@ -0,0 +1,9 @@
+def parse_menus_data(menus_info):
+	products = {}
+	root_ids = []
+
+	for product in menus_info['menus']:
+		if not 'parent_id' in product:
+			root_ids.append(product['id'])
+		products[product['id']] = product
+	return products, root_ids
