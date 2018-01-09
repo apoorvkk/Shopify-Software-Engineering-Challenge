b6b335e2eb70e099705dbce7eb2105b89ee1557d
Apoorv Kansal <akansal@zendesk.com>
2018-01-09 17:28:45 +1100

    Minor linting fixes and modified travis.yml to run tests in correct location

--- MenuValidator/backendwebapp/views.py
+++ MenuValidator/backendwebapp/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from rest_framework import status
 import cli.menus as menus
 
 
@@ -11,6 +9,5 @@ def fetch_menus(request):
     menus_info = menus.fetch_menus_data(problem_id)
     products, root_ids = menus.parse_menus_data(menus_info)
     valid_menus, invalid_menus = menus.validate_menus(root_ids, products)
-    return HttpResponse(menus.render_aggregate_menus_json(valid_menus, invalid_menus), content_type='application/json')
-
-
+    menu_data = menus.render_aggregate_menus_json(valid_menus, invalid_menus)
+    return HttpResponse(menu_data, content_type='application/json')
